cb2faa032790d3c67ac630a6e09b273551a27cd7
Hesam <Hesam.Shaelaie@gmail.con>
2021-06-24 23:37:37 -0400

    typo fixed

--- docs/src/examples/PHTP_example1.jl
+++ docs/src/examples/PHTP_example1.jl
@@ -9,13 +9,8 @@
 # ```math
 # \min (x-5)^2+(2y+1)^2,\\
 # \notag s.t.\\
-<<<<<<< HEAD
 # x \geq 0,\\
 # y \geq 0,\\
-=======
-# x \qeq 0,\\
-# y \qeq 0,\\
->>>>>>> 4c4473548e37dbbdce4d406b7117bc059acddb63
 # ```
 # Second level
 # ```math
