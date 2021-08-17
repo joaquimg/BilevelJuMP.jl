935f8ec9ef7f6c4cf454f47d1c1438c1aa992f7a
Documenter.jl <documenter@juliadocs.github.io>
2021-08-17 20:56:39 +0000

    build based on dcdcc56

---
+++ dev/examples/MibS_example1.jl
@@ -0,0 +1,48 @@
+# # Example on MibS
+# This example is from ????
+
+
+# Model of the problem
+# First level
+# ```math
+# \min_{x} -3x -7y,\\
+# \notag s.t.\\
+# -3x + 2y \leq 12,\\ 
+# x + 2y \leq 20,\\
+# x \leq 10,\\
+# x \in \mathbb{Z},\\
+# ```
+# Second level
+# ```math
+# \min_{y} y,\\
+# \notag s.t.\\
+# 2x - y <= 7,\\
+# -2x + 4y <= 16,\\
+# y <= 5\\
+# y \in \mathbb{Z}\\
+# ```
+
+using BilevelJuMP
+using JuMP
+using Test
+
+model = BilevelModel()
+@variable(Upper(model), x, Int)
+@variable(Lower(model), y, Int)
+@objective(Upper(model), Min, -3x - 7y)
+@constraints(Upper(model), begin
+    u1, -3x + 2y <= 12
+    u2, x + 2y <= 20
+    u3, x <= 10
+end)
+@objective(Lower(model), Min, y)
+@constraint(Lower(model), l1,  2x -  y <= 7)
+@constraint(Lower(model), l2, -2x +  4y <= 16)
+@constraint(Lower(model), l3, y <= 5)
+solution = BilevelJuMP.solve_with_MibS(model, silent = true)
+@test solution.status == true
+@test solution.objective ≈ -53
+@test solution.nonzero_upper == Dict(0 => 6.0)
+@test solution.nonzero_lower == Dict(0 => 5.0)
+@test solution.all_upper["x"] == 6.0
+@test solution.all_lower["y"] == 5.0
