0f4d5cbd10bd241a518b38a3b3d25282f8786821
Documenter.jl <documenter@juliadocs.github.io>
2021-08-18 21:43:17 +0000

    build based on d89df2c

--- dev/examples/MibS_example1.jl
+++ dev/examples/MibS_example1.jl
@@ -27,19 +27,42 @@ using JuMP
 using Test
 
 model = BilevelModel()
+
+# First we need to create all of the variables in the upper and lower problems:
+
+# Upper level variables
 @variable(Upper(model), x, Int)
+
+#Lower level variables
 @variable(Lower(model), y, Int)
+
+# Then we can add the objective and constraints of the upper problem:
+
+# Upper level objecive function
 @objective(Upper(model), Min, -3x - 7y)
+
+# Upper constraints
 @constraints(Upper(model), begin
     u1, -3x + 2y <= 12
     u2, x + 2y <= 20
     u3, x <= 10
 end)
+
+# Followed by the objective and constraints of the lower problem:
+
+# Lower objective function
 @objective(Lower(model), Min, y)
+
+# Lower constraints
 @constraint(Lower(model), l1,  2x -  y <= 7)
 @constraint(Lower(model), l2, -2x +  4y <= 16)
 @constraint(Lower(model), l3, y <= 5)
+
+# Using MibS Solver
 solution = BilevelJuMP.solve_with_MibS(model, silent = true)
+
+
+# Auto testing
 @test solution.status == true
 @test solution.objective ≈ -53
 @test solution.nonzero_upper == Dict(0 => 6.0)
