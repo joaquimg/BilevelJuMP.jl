d17f5e9ae7e28c5bcd03694a1479f62b4191625b
Nick Laws <laws.nick@gmail.com>
2022-04-20 21:20:46 -0600

    update IEEE paper link

--- src/bilinear_linearization.jl
+++ src/bilinear_linearization.jl
@@ -1,9 +1,7 @@
 #=
 Methods in this file are for supporting the linearization of bilinear products of lower dual and 
 lower primal variables in the upper level problem. 
-Details can be found in http://www.optimization-online.org/DB_HTML/2021/08/8561.html
-
-TODO: update paper reference once published
+Details can be found in https://ieeexplore.ieee.org/abstract/document/9729553
 =#
 
 """
