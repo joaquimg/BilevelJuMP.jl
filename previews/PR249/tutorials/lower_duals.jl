82563edefd5da8f93f09b71b4199692bd3c9d2e9
Documenter.jl <documenter@juliadocs.github.io>
2026-08-22 21:55:47 +0000

    build based on c02370a

--- previews/PR249/tutorials/lower_duals.jl
+++ previews/PR249/tutorials/lower_duals.jl
@@ -1,17 +1,25 @@
 # # Dual variables of the lower level
 
-# This is a quick introduction to modeling and solving bilevel optimization
-# with BilevelJuMP.
+# BilevelJuMP supports use of duals of lower-problem constraints as variables
+# in the upper problem via the `DualOf()` function.
 
-# This modeling feature enables the implementation of workflows where one
-# (or more) of the upper-level variables is the dual of a lower-level
-# constraint. In particular, in the energy sector, it is common to model the
-# energy prices as the dual variable associated with the energy demand
-# equilibrium constraint. One example of an application that uses this feature
-# is [Fanzeres et al. (2019)](https://doi.org/10.1016/j.ejor.2018.07.027),
-# which focuses on strategic bidding in
-# auction-based energy markets. A small and simplified example of the modeled
-# problem would be the model:
+# `DualOf()` takes a named constraint from the lower problem as an argument.
+# It is used in the `expr` argument of the `@variable` JuMP macro:
+
+# ```julia
+# @constraint(Lower(model), some_constraint, x <= y)
+#
+# @variable(Upper(model), lambda, DualOf(some_constraint))
+# ```
+
+# ## Example: strategic bidding in an energy market 
+
+# In the energy sector, it is common to model market prices as the dual of the
+# demand equilibrium constraint in the lower-level economic dispatch problem.
+# One example of this approach is found in
+# [Fanzeres et al. (2019)](https://doi.org/10.1016/j.ejor.2018.07.027),
+# which focuses on strategic energy offers in auction-based energy markets. A
+# simplified example of the model is:
 
 # ```math
 # \begin{align}
@@ -27,48 +35,58 @@
 # \end{align}
 # ```
 
-# Where $\lambda$ is the dual of the load balance constraint
-# (last constraint in the lower part),
-# $g_S$, $g_{R1}$, $g_{R2}$ represent the generation of
-# the strategic bidder and from two other (non-strategic) plants.
-# $g_D$ represents the deficit in generation.
-# Finally, $q_S$ is the quantity bid optimized by the strategic generator.
+# where:
+#  * $S$ is the strategically-bidding asset controlled by the upper-problem
+#    agent, where:
+#    * $q_S$ is the quantity of generation offered into the market, to be
+#      optimized by the upper-problem agent to maximize revenue, and
+#    * $g_S$ is the quantity of generation dispatched by the lower-problem
+#      system operator, which is no greater than $q_S$
+#  * $g_1$ and $g_2$ are the generation of two other non-strategic,
+#    price-taking generators;
+#  * $g_D$ is the deficit in generation; and
+#  * $\lambda$ is the dual of the load balance constraint
 
-# load packages
+# To implement this model in BilevelJuMP, first load the necessary packages:
 
 using BilevelJuMP
 using Ipopt
 using QuadraticToBinary
 using HiGHS
 
-# BilevelJuMP.jl allows users to implement similar models using the
-# function `DualOf` that binds a new variable in the upper level
-# to an existing constraint in the lower level.
-# The model can be written as:
+# Instantiate the model and fully describe the lower problem:
 
 model = BilevelModel()
+
 @variable(Upper(model), 0 <= qS <= 100)
+
 @variable(Lower(model), 0 <= gS <= 100)
-@variable(Lower(model), 0 <= gR1 <= 40)
-@variable(Lower(model), 0 <= gR2 <= 40)
+@variable(Lower(model), 0 <= g1 <= 40)
+@variable(Lower(model), 0 <= g2 <= 40)
 @variable(Lower(model), 0 <= gD <= 100)
-@objective(Lower(model), Min, 50gR1 + 100gR2 + 1000gD)
+
+@objective(Lower(model), Min, 50g1 + 100g2 + 1000gD)
+
 @constraint(Lower(model), gS <= qS)
-@constraint(Lower(model), demand_equilibrium, gS + gR1 + gR2 + gD == 100)
+@constraint(Lower(model), demand_equilibrium, gS + g1 + g2 + gD == 100)
+
+# The BilevelJuMP.jl function `DualOf()` binds a new variable in the upper
+# level to an existing constraint in the lower level:
+
 @variable(Upper(model), lambda, DualOf(demand_equilibrium))
 @objective(Upper(model), Max, lambda*gS)
 
-# ## NLP solution
+# ### NLP solution
 
-# This model, can be solved by selecting a reformulation and a solver.
-# Here we select Strong-Duality reformulation, the Ipopt solver and call
-# `optimize!` to perform the reformulation and solve it.
+# This model can be solved by selecting a reformulation and a solver.
+# Here we select Strong-Duality reformulation and the Ipopt solver, and call
+# `optimize!()` to perform the reformulation and solve it.
 
 BilevelJuMP.set_mode(model, BilevelJuMP.StrongDualityMode())
 set_optimizer(model, Ipopt.Optimizer)
 optimize!(model)
 
-# ## MIP solution
+# ### MIP solution
 
 # It is also possible to solve such problem by using a MIP formulation.
 # The main issue is the product of variables in the upper level objective.
@@ -90,17 +108,21 @@ BilevelJuMP.set_mode(model,
     BilevelJuMP.FortunyAmatMcCarlMode(dual_big_M = 100))
 optimize!(model)
 
-# ## More on `DualOf` usage
+# ## Using `DualOf()` with vectors of constraints
 
-# You might have a problem where you want duals of a vector of constraints like:
+# `DualOf()` can also be used to create a vector of variables in the upper
+# problem based on a vector of named constraints in the lower problem.
 
-@constraint(Lower(model), reserves[i=1:3], (40 - gR1) + (40 - gR2) == 10 * i)
+# `DualOf()` requires a named constraint as an input, for example:
 
-# then you can do
+@constraint(Lower(model), reserves[i=1:3], (40 - g1) + (40 - g2) == 10 * i)
+
+# You can use `DualOf()` with the built-in JuMP method for creating vectors of
+# named variables:
 
 @variable(Upper(model), reserve_dual[i=1:3], DualOf(reserves[i]))
 
-# or use anonymous variables
+# You can also use `DualOf()` to create a vector of anonymous variables:
 
 my_duals = []
 for i in 1:3
